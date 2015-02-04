222004c085e8b60668942141b554ce0e14078754
Ahmet ATAY <ahmet.atay@hotmail.com>
2015-02-04 18:15:17 +0200

    Active development

---
+++ Layout/Filesystem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Layout;
+
+class Filesystem {
+
+	public static function put()
+	{
+
+	}
+}
---
+++ Layout/test.php
@@ -0,0 +1,8 @@
+<?php if( $test == 'Ahmet' && true === true ): ?>
+                    Meraba Ahmet 
+                <?php endif; ?>
+
+                <?php if($test == 'Mehmet'): ?>
+                    Meraba Mehmet 
+                <?php endif; ?>
+                
